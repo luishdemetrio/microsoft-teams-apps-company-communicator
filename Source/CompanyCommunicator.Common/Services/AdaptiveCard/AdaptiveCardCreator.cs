525e4e49fb19319713dbb9b65b65a722108216ea
Luis Henrique Demetrio <luisdem@microsoft.com>
2023-09-21 18:23:54 -0300

    fix of the issue #57

--- Source/CompanyCommunicator.Common/Services/AdaptiveCard/AdaptiveCardCreator.cs
+++ Source/CompanyCommunicator.Common/Services/AdaptiveCard/AdaptiveCardCreator.cs
@@ -169,6 +169,15 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.AdaptiveCard
                     IsVisible = true,
                     AltText = string.Empty,
                 });
+
+                //card.Body.Add(new AdaptiveTextBlock()
+                //{
+                //    Text = new Uri(trul, UriKind.RelativeOrAbsolute).ToString(),
+                //    Size = AdaptiveTextSize.ExtraLarge,
+                //    Weight = AdaptiveTextWeight.Bolder,
+                //    Wrap = true,
+                //    Separator = true,
+                //});
             }
 
             return card;
